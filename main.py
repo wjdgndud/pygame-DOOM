a90f26645a3036b9c5b4edf3d9d38588b610f8df
wjdgndud <102457583+wjdgndud@users.noreply.github.com>
2023-11-30 12:32:08 +0900

    Sprites

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from map import *
 from player import *
 from raycasting import *
 from object_renderer import *
+from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
 
 
 class Game:
@@ -21,17 +25,22 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
+        self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        # self.screen.fill('black')
         self.object_renderer.draw()
+        self.weapon.draw()
         # self.map.draw()
         # self.player.draw()
 
@@ -40,6 +49,8 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(event)
+
 
     def run(self):
         while True:
